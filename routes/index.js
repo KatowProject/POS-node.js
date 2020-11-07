35eb60b003ba56bcf2ce8a00baba95ccc3e7ff72
Kato <52640561+KatowProject@users.noreply.github.com>
2020-11-07 16:21:19 +0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -4,19 +4,6 @@ var path = require('path');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  let staff = [{
-    facebook: {
-      headAdmin: [],
-      admin: [],
-      moderator: []
-    },
-    discord: {
-      headAdmin: [],
-      admin: [],
-      moderator: [],
-      botManager: []
-    }
-  }]
   res.render('index', { facebook: staff.facebook, discord: staff.discord });
 });
 
